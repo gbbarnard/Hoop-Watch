d37f114f155969701d359b23c487babd6ac09a23
gbbarnard <114040561+gbbarnard@users.noreply.github.com>
2026-02-14 15:34:14 -0500

    Initial commit

---
+++ Hoop-Watch-main/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    port: 3000
+  }
+};
